74756bce2b7e5a46e3231f8893ba8408b757b4c6
rondpunt <vdg.niels@gmail.com>
2026-08-15 14:39:23 +0200

    android: remove legacy Lovable references from Capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,12 +4,6 @@ const config: CapacitorConfig = {
   appId: 'be.shopgo.kortrijk',
   appName: 'Shop&Go Kortrijk',
   webDir: 'dist',
-  // Hot-reload vanuit Lovable preview tijdens dev. Verwijder/comment dit blok
-  // vóór je een productie-APK bouwt, anders laadt de APK altijd de preview.
-  // server: {
-  //   url: 'https://026b6afd-cf2f-4531-b26b-dcf88271c2e8.lovableproject.com?forceHideBadge=true',
-  //   cleartext: true,
-  // },
   android: {
     backgroundColor: '#FAF6F0',
   },
